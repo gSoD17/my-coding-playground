d571e1697ff3ef415800b8455a43819193b55116
Gino17 <galentro451@gmail.com>
2018-11-19 18:19:52 -0800

    added jquery code to script to check for changes in text area, commented out other text areas

--- src/js/script.js
+++ src/js/script.js
@@ -6,12 +6,18 @@ $('.button-coding-view').hover(function() {
 
     $(this).removeClass('button-highlight-effect');
 
-})
+});
 
 $('.button-coding-view').click(function() {
 
     $(this).toggleClass('button-active');
 
     $(this).removeClass('button-highlight-effect');
+
+});
+
+$('textarea').on('change keyup paste', function() {
+
+    $('iframe').contents().find('html').html($('#coding-space-html').val());
 
 })
