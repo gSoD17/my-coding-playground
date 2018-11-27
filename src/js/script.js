543f2460a083372e766a73f52e3a087c2726c9f4
Gino17 <galentro451@gmail.com>
2018-11-26 17:18:12 -0800

    updated code that tracks javascript panel to execute in iframe

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ function updateOutput() {
 
     $('iframe').contents().find('html').html('<html><head><style type="text/css">' + $('#cssPanel').val() + '</style></head><body>' + $('#htmlPanel').val() + '</body></html>');
 
-    eval($('#javascriptPanel').val());
+    document.getElementById('outputPanel').contentWindow.eval($('#javascriptPanel').val());
 
 }
 
